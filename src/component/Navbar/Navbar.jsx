dbf60d658b836e0a504c635b227ef7a38e1d7cc9
mralimalik <aliriazahmad72@gmail.com>
2024-12-23 13:48:52 +0500

    fix

--- src/component/Navbar/Navbar.jsx
+++ src/component/Navbar/Navbar.jsx
@@ -4,17 +4,26 @@ import "./Navbar.css";
 import { FiLogOut } from "react-icons/fi";
 import { toast } from "react-toastify";
 import { useNavigate } from "react-router-dom";
-
+import { AuthContext } from "../../context/AuthContext";
+import { useContext } from "react";
 const Navbar = ({ toggleSidebar }) => {
   const navigate = useNavigate();
+  const { userData } = useContext(AuthContext);
   const handleLogout = () => {
     localStorage.removeItem("Token");
     navigate("/login");
   };
 
-  const handleProfileNavigate = ()=>{
-    navigate('/account/profile');
-  }
+  const handleProfileNavigate = () => {
+    navigate("/account/profile");
+  };
+  const getLettersFromName = () => {
+    if (!userData) return "";
+    const { firstName, lastName } = userData;
+    const firstInitial = firstName?.charAt(0).toUpperCase() || "";
+    const lastInitial = lastName?.charAt(0).toUpperCase() || "";
+    return `${firstInitial}${lastInitial}`;
+  };
 
   return (
     <div className="navbar-items w-full bg-white px-4 py-2 flex justify-end items-center gap-2">
@@ -35,14 +44,13 @@ const Navbar = ({ toggleSidebar }) => {
           <FiLogOut />
           <h2 className="pr-2">Logout</h2>
         </div>
-        {/* <input
-          className="w-[300px] p-1 border outline-none rounded-[10px] "
-          type="search"
-          id="search"
-          placeholder="Search"
-          required
-        /> */}
-        <div className="account-avatar" onClick={handleProfileNavigate}></div>
+
+        <div
+          className="account-avatar flex items-center justify-center bg-violet-500 text-white rounded-full w-8 h-8 font-bold"
+          onClick={handleProfileNavigate}
+        >
+          {getLettersFromName()}
+        </div>
       </div>
     </div>
   );
--- src/pages/Dashboard/Dashboard.jsx
+++ src/pages/Dashboard/Dashboard.jsx
@@ -19,68 +19,68 @@ const Dashboard = () => {
     : "#";
   return (
     <div className="main-dashboard flex gap-3">
-   <div>
-   <div className="welcome-div">
-        <p className="date-text">{formattedDate}</p>
-        <p className="current-user-text">
-  {Object.keys(userData).length === 0 ? `${userData.email},` : ""}Welcome
-</p>
-      </div>
-      <div className="flex gap-4 responsive-div">
-        <div className="dashboard-qr-container">
-          <div className="qr-text-left">
-            <span className="text-2xl">🎉</span>
-            <h2 className="text-2xl">
-              {selectedVenue
-                ? "Your digital menu is ready!"
-                : "To Continue Create Venue"}
-            </h2>
+      <div>
+        <div className="welcome-div">
+          <p className="date-text">{formattedDate}</p>
+          <p className="current-user-text">
+            {Object.keys(userData).length > 0 ? `${userData.email},` : ""}
+            Welcome
+          </p>
+        </div>
+        <div className="flex gap-4 responsive-div">
+          <div className="dashboard-qr-container">
+            <div className="qr-text-left">
+              <span className="text-2xl">🎉</span>
+              <h2 className="text-2xl">
+                {selectedVenue
+                  ? "Your digital menu is ready!"
+                  : "To Continue Create Venue"}
+              </h2>
 
-            <h4>
-              {selectedVenue
-                ? "Scan the QR code or use the link to view your brand new menu."
-                : "Get your QR code by creating new venue"}
-            </h4>
-            {/* <button
+              <h4>
+                {selectedVenue
+                  ? "Scan the QR code or use the link to view your brand new menu."
+                  : "Get your QR code by creating new venue"}
+              </h4>
+              {/* <button
             className="preview-button"
             onClick={() => `https://qr-menu-7ie2.vercel.app/${selectedVenue?.venueId} `}
           >
             Preview
           </button> */}
-            <a
-              href={previewLink}
-              target="_blank"
-              rel="noopener noreferrer"
-              className={`preview-button`}
-              onClick={(e) => {
-                if (!selectedVenue) {
-                  e.preventDefault(); // Prevent navigation if no venue is selected
-                  alert("Please create a venue first.");
-                }
-              }}
-            >
-              Preview
-            </a>
-          </div>
-
-          <div className="phone-qr-position-div">
-            <div className="scan-me-img">
-              <img src={ScanMe} alt="" className="scanme-image" />
+              <a
+                href={previewLink}
+                target="_blank"
+                rel="noopener noreferrer"
+                className={`preview-button`}
+                onClick={(e) => {
+                  if (!selectedVenue) {
+                    e.preventDefault(); // Prevent navigation if no venue is selected
+                    alert("Please create a venue first.");
+                  }
+                }}
+              >
+                Preview
+              </a>
             </div>
 
-            <div className="qr-code-div">
-              <QrCode />
-            </div>
+            <div className="phone-qr-position-div">
+              <div className="scan-me-img">
+                <img src={ScanMe} alt="" className="scanme-image" />
+              </div>
 
-            <div className="phome-img-div">
-              <img src={Phone} alt="" className="phone-img" />
+              <div className="qr-code-div">
+                <QrCode />
+              </div>
+
+              <div className="phome-img-div">
+                <img src={Phone} alt="" className="phone-img" />
+              </div>
             </div>
           </div>
         </div>
-   </div>
       </div>
       {selectedVenue && <PagePreview venueId={selectedVenue.venueId} />}
-
     </div>
   );
 };
